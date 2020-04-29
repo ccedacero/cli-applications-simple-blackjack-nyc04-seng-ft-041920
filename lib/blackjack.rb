64e0162591a65998907e24005225c8a000a3a243
Cristian Cedacero <cristian.cedacero@gmail.com>
2020-04-29 23:14:03 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,44 +1,65 @@
-def welcome
-  # code #welcome here
+def welcome() 
+  puts 'Welcome to the Blackjack Table'
 end
 
-def deal_card
-  # code #deal_card here
+def deal_card()
+  rand(1..11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(num)
+puts "Your cards add up to #{num}"
 end
 
-def prompt_user
-  # code #prompt_user here
+def prompt_user()
+  puts "Type 'h' to hit or 's' to stay"
 end
 
-def get_user_input
-  # code #get_user_input here
+def get_user_input()
+ gets.chomp
 end
 
-def end_game
-  # code #end_game here
+
+def end_game(num)
+ puts "Sorry, you hit #{num}. Thanks for playing!"
 end
 
-def initial_round
-  # code #initial_round here
+
+def initial_round()
+  sum = deal_card()
+  sum2 = deal_card()
+  total = sum + sum2
+  display_card_total(total)
+  return total 
 end
 
-def hit?
-  # code hit? here
+def invalid_command()
+  puts "Please enter a valid command"
 end
 
-def invalid_command
-  # code invalid_command here
+
+def hit?(num)
+prompt_user
+input = get_user_input
+if input == 'h'
+  num += deal_card()
+elsif input == 's'
+nil
+else
+invalid_command()
+hit?(num)
+end 
+num
 end
 
-#####################################################
-# get every test to pass before coding runner below #
-#####################################################
+ def runner()
+ welcome()
+ num = initial_round()
+ hit?(num)
+ display_card_total(num)
+ while num <= 21 
+   num = hit?(num)
+  display_card_total(num)
+  end
+  end_game(num)
+  end
 
-def runner
-  # code runner here
-end
-    
